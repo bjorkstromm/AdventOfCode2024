c574af42afcafd2621e5ffdfe1c1c65d4e480ac8
Martin Björkström <martin@bjorkstrom.dev>
2024-12-01 23:30:53 +0200

    Day 01 - Part 2

--- day01.fsx
+++ day01.fsx
@@ -26,3 +26,25 @@ let part1 filename =
     |> sort
     |> Array.map distance
     |> Array.sum
+
+// Part 2
+
+let similarityScore xmap x =
+    match xmap |> Map.tryFind x with
+    | Some c -> (uint64)c * (uint64)x
+    | None -> 0UL
+
+let part2 filename =
+    let (xs, ys) =
+        filename
+        |> parseFile
+        |> Array.unzip
+
+    let xmap =
+        ys
+        |> Array.countBy id
+        |> Map.ofArray
+
+    xs
+    |> Array.map (similarityScore xmap)
+    |> Array.sum
