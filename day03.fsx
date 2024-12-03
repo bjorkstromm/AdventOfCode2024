c075b82780bd1fa80a1c6a9963af9c2755754fe9
Martin Björkström <martin@bjorkstrom.dev>
2024-12-03 09:40:00 +0200

    Day 03 - Part 2

--- day03.fsx
+++ day03.fsx
@@ -1,5 +1,7 @@
 type Instruction =
     | Multiply of int * int
+    | DoIns
+    | DontIns
 
 let (|Integer|_|) (str: string) =
    let mutable intvalue = 0
@@ -17,10 +19,22 @@ let (|Mul|_|) (str: string) =
    | ParseRegex @"^mul\((-?\d+),\s*(-?\d+)\)" ([Integer w; Integer h], rest) -> Some (Multiply (w, h), rest)
    | _ -> None
 
+let (|Dont|_|) (str: string) =
+   match str with
+   | ParseRegex @"^don't\(\)" ([], rest) -> Some (DontIns, rest)
+   | _ -> None
+
+let (|Do|_|) (str: string) =
+   match str with
+   | ParseRegex @"^do\(\)" ([], rest) -> Some (DoIns, rest)
+   | _ -> None
+
 let rec parse input =
     match input with
     | "" -> []
-    | Mul (instr, rest) -> instr :: parse rest
+    | Mul (instr, rest)
+    | Do (instr, rest)
+    | Dont (instr, rest) -> instr :: parse rest
     | _ -> parse (input.Substring(1))
 
 
@@ -31,4 +45,18 @@ let part1 filename =
     |> List.sumBy (fun instr ->
         match instr with
         | Multiply (x, y) -> x * y
+        | _ -> 0
     )
+
+let part2 filename =
+    let folder (enabled, sum) instr =
+        match instr with
+        | DoIns -> (true, sum)
+        | DontIns -> (false, sum)
+        | Multiply (x, y) when enabled -> (enabled, sum + (x * y))
+        | _ -> (enabled, sum)
+
+    filename
+    |> System.IO.File.ReadAllText
+    |> parse
+    |> List.fold folder (true, 0)
