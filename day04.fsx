8299266c316ee0bb71c5b777f5daf7d6be2f6fe4
Martin Björkström <martin@bjorkstrom.dev>
2024-12-04 22:38:44 +0200

    Day 04 - Part 2

--- day04.fsx
+++ day04.fsx
@@ -35,3 +35,42 @@ let part1 filename =
     |> Seq.collect (fun (x, y) -> sliceFromIndex arr 4 (x, y))
     |> Seq.filter (fun slice -> slice = str)
     |> Seq.length
+
+// Part 2
+let extractSquare (x, y) size (arr : 'a[,]) =
+    Array2D.init size size (fun x' y' -> arr.[x + x', y + y'])
+
+let extractSquares size arr =
+    seq {
+        for x in 0 .. Array2D.length1 arr - size do
+            for y in 0 .. Array2D.length2 arr - size do
+                yield (x, y)
+    }
+    |> Seq.map (fun (x, y) -> extractSquare (x, y) size arr)
+
+let extractDiagonals arr =
+    let rows = Array2D.length1 arr
+    let cols = Array2D.length2 arr
+    let size = min rows cols
+    [|
+        [| for i in 0 .. size - 1 -> arr.[i, i] |]
+        [| for i in 0 .. size - 1 -> arr.[i, size - i - 1] |]
+    |]
+
+let part2 filename =
+    let valid =
+        [|"MAS".ToCharArray(); "SAM".ToCharArray() |]
+        |> Set.ofArray
+
+    let isValid slice =
+        valid |> Set.contains slice
+
+    filename
+    |> parseFile
+    |> extractSquares 3
+    |> Seq.map (fun square ->
+        square
+        |> extractDiagonals
+        |> Seq.filter isValid)
+    |> Seq.filter (Seq.length >> (=) 2)
+    |> Seq.length
